ec20726737ffeb9fdc773c9b48cca8def5a65681
renanfschmidt <rschmidt13@mail.valenciacollege.edu>
2026-01-25 14:58:11 -0500

    commit
    commit to main

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 print("Hello World")
+#using the hashtag symbol is a way to make comments
+print("Hello hows it going")
+
+name="scooby"#variables unlike other languages dont have a type
+#int(whole number), double(double the float) ,float(decimal number), char( characters ),string (words), boolean(true or false)
+# == this is the value for equals
+age = 23
+temperature = 98.7
+print("I am {} years old ,and the temparature is {} degrees".format(age,temperature))
+#input
+friendName = input("Whats your name?\n ")
+print(friendName)
+fage = int(input("Whats your friends age?\n "))
+print(fage)
+total = 56+78
+print(total)
+agesTotal = fage+age
+print(agesTotal)
+modulusTotal= 5 % 2
+print(modulusTotal)
