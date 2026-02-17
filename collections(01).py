b5a847f6b08d764a180f8819cdc45c3c5187ba50
renanfschmidt <rschmidt13@mail.valenciacollege.edu>
2026-02-16 22:24:28 -0500

    Update collections(01).py
    new

--- collections(01).py
+++ collections(01).py
@@ -5,17 +5,19 @@ Gradelist_1.append(int(input("Give me the second grade:\n")))
 Gradelist_1.append(int(input("Give me the third grade:\n")))
 Gradelist_1.append(int(input("Give me the fourth grade:\n")))
 Gradelist_1.append(int(input("Give me the fifth grade:\n")))
-averagegrade=(Gradelist_1[0]+Gradelist_1[1]+Gradelist_1[2]+Gradelist_1[3]+Gradelist_1[4])/len(Gradelist_1)
 print("-"*50)
 print(name_1)
-print("The average grade is ",averagegrade)
-if averagegrade>=90:
- print("You got an A")
-elif averagegrade>=80:
- print("You got a B")
-elif averagegrade>=70:
- print("You got a C")
-elif averagegrade>=60:
- print("You got a D")
+def gradename():
+ averagegrade = (Gradelist_1[0] + Gradelist_1[1] + Gradelist_1[2] + Gradelist_1[3] + Gradelist_1[4]) / len(Gradelist_1)
+ return averagegrade
+print("The average grade is ", gradename())
+if gradename()>=90:
+  print("You got an A")
+elif gradename()>=80:
+  print("You got a B")
+elif gradename()>=70:
+  print("You got a C")
+elif gradename()>=60:
+  print("You got a D")
 else:
- print("You got an F")
+  print("You got an F")
