1c8638c5e04554a9da5f1139e69fe6dc401b84c0
renanfschmidt <rschmidt13@mail.valenciacollege.edu>
2026-03-15 22:08:49 -0400

    Update snippet_1.py

--- snippet_1.py
+++ snippet_1.py
@@ -2,3 +2,10 @@ with open("output.txt", "a") as db:
     a = "Hello" + str(1)
     b = "How do you do?"
     db.write(a + ", " + b + "\n")
+
+def findTotal(a):
+ for i in a:
+  print(sum(i)*2)
+findTotal([[5,10],[10,30]])
+
+Hello1, How do you do?
